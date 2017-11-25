39f109a139405fe0682526e0cfdc9eba11d19037
Dell <Dell@Dell-PC>
2017-11-25 17:44:43 +0530

    committed

--- javasample/src/main/java/com/code/javasample/javasample/App.java
+++ javasample/src/main/java/com/code/javasample/javasample/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!543543 First **" );
-        System.out.println( "******111111111111" );
+        System.out.println( "******" );
     }
 }
