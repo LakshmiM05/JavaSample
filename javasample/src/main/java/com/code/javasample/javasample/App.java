721f3725959474451e14e110c5c231dc10a02a43
Dell <Dell@Dell-PC>
2017-11-25 18:01:48 +0530

    resolved conflict-ws2

--- javasample/src/main/java/com/code/javasample/javasample/App.java
+++ javasample/src/main/java/com/code/javasample/javasample/App.java
@@ -9,10 +9,10 @@ public class App
     public static void main( String[] args )
     {      
         System.out.println( "Hello World!543543 First **" );
-<<<<<<< HEAD
+
         System.out.println( "******" );
-=======
-        System.out.println( "111111111111" );
->>>>>>> branch 'master' of https://github.com/LakshmiM05/JavaSample
+       // System.out.println( "111111111111" );
+
+       
     }
 }
